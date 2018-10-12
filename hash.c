5df7075f5b84b12ff9a15881d050a65179a284cf
JulianVentura <julianv6004@gmail.com>
2018-10-11 21:50:40 -0300

    Agrego funcion hash

--- hash.c
+++ hash.c
@@ -6,17 +6,79 @@
  *                DEFINICION DE LOS TIPOS DE DATOS
  * *****************************************************************/
 
-struct hash;
-struct hash_iter;
-//No se por que hay dos strucs hash pero esta asi en el .h
-typedef struct hash hash_t;
-typedef struct hash_iter hash_iter_t;
+ typedef struct hash_campo{
+     char *clave;
+     void *valor;
+     estado_t estado;
+ }hash_campo_t;
 
-typedef void (*hash_destruir_dato_t)(void *);
+ struct hash {
+     size_t cantidad;
+     size_t largo;
+     size_t carga;
+     hash_campo_t *tabla;
+     hash_destruir_dato_t destruir_dato;
+ };
+
+ /* ******************************************************************
+  *                      PRIMITIVAS PRIVADAS
+  * *****************************************************************/
+//https://stackoverflow.com/questions/32795453/use-of-murmurhash-in-c
+uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed) {
+      static const uint32_t c1 = 0xcc9e2d51;
+      static const uint32_t c2 = 0x1b873593;
+      static const uint32_t r1 = 15;
+      static const uint32_t r2 = 13;
+      static const uint32_t m = 5;
+      static const uint32_t n = 0xe6546b64;
+
+      uint32_t hash = seed;
+
+      const int nblocks = len / 4;
+      const uint32_t *blocks = (const uint32_t *) key;
+      int i;
+      for (i = 0; i < nblocks; i++) {
+          uint32_t k = blocks[i];
+          k *= c1;
+          k = (k << r1) | (k >> (32 - r1));
+          k *= c2;
+
+          hash ^= k;
+          hash = ((hash << r2) | (hash >> (32 - r2))) * m + n;
+      }
+
+      const uint8_t *tail = (const uint8_t *) (key + nblocks * 4);
+      uint32_t k1 = 0;
+
+      switch (len & 3) {
+      case 3:
+          k1 ^= tail[2] << 16;
+      case 2:
+          k1 ^= tail[1] << 8;
+      case 1:
+          k1 ^= tail[0];
+
+          k1 *= c1;
+          k1 = (k1 << r1) | (k1 >> (32 - r1));
+          k1 *= c2;
+          hash ^= k1;
+      }
+   hash ^= len;
+   hash ^= (hash >> 16);
+   hash *= 0x85ebca6b;
+   hash ^= (hash >> 13);
+   hash *= 0xc2b2ae35;
+   hash ^= (hash >> 16);
+
+   return hash;
+}
+
+
 
 /* ******************************************************************
  *                      PRIMITIVAS DEL HASH
  * *****************************************************************/
+
 
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato);
 

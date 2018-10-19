4b9441653b7b2a866d7a18c73014e1ca0ba525e2
JulianVentura <julianv6004@gmail.com>
2018-10-19 13:22:12 -0300

    Agrego nuevo_largo(); y redimension disminuyendo tamaño

--- hashjulian.c
+++ hashjulian.c
@@ -4,11 +4,15 @@
 #include <stddef.h>
 #include <string.h>
 
-#define TAM 33
-#define FACTOR_LIMITE 0.2
+#define TAM 37
+#define FACTOR_AUMENTO 0.7
+#define FACTOR_DISMINUCION 0.2
 #define VACIO 'V'
 #define OCUPADO 'O'
 #define BORRADO 'B'
+#define AUMENTAR true
+#define DISMINUIR false
+#define TAM_PRIMOS 26
 
 /* ******************************************************************
  *                DEFINICION DE LOS TIPOS DE DATOS
@@ -132,9 +136,33 @@ void inicializar_tabla(hash_campo_t *tabla, size_t tam){
 //FUNCION DE REDIMENSION
 ////
 
-bool redimensionar_hash(hash_t *hash){
+size_t nuevo_largo(hash_t *hash, bool aumentar){
+    size_t vector[TAM_PRIMOS] = {37, 79, 163, 331, 673, 1361, 2729, 5471, 10949, 21911, 43853, 87719, 175447, 350899, 701819, 1403641, 2807303, 5614657, 11229331, 22458671, 449117381, 89834777, 179669557, 359339171, 718678369, 1437356741};//Primos
+    int i=0;
+    if(hash->largo >= vector[TAM_PRIMOS-1]){
+        if(aumentar) return hash->largo*2;
+        return hash->largo/2;
+    }
+    for(i=0; i<TAM_PRIMOS; i++){
+        if(vector[i] == hash->largo){
+            break;
+        }
+    }
 
-    size_t tam_nuevo = hash->largo*3; //Acá podemos llamar a una funcion que devuelva un numero primo como tam_nuevo
+    if(aumentar){
+        i++;
+    }else{
+        i -= 2;
+    }
+    if(i >= TAM_PRIMOS) return hash->largo*2;
+    if(i<0) return vector[0];
+
+    return vector[i];
+}
+
+bool redimensionar_hash(hash_t *hash, bool aumentar){
+
+    size_t tam_nuevo = nuevo_largo(hash, aumentar); //Le pasamos false para disminuir el largo y true para aumentar
     hash_campo_t* tabla_nueva = malloc(sizeof(hash_campo_t)*tam_nuevo);
     size_t tope = hash->largo;
     if(!tabla_nueva) return false;
@@ -190,7 +218,7 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
     hash_t* hash = malloc(sizeof(hash_t));
     if (!hash) return NULL;
 
-    hash_campo_t* tabla = malloc(sizeof(hash_campo_t)*TAM); //#define TAM 33
+    hash_campo_t* tabla = malloc(sizeof(hash_campo_t)*TAM);
 
     if (!tabla){
         free(hash);
@@ -209,8 +237,8 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 
-    if(hash->carga >= FACTOR_LIMITE){
-        if(!redimensionar_hash(hash)) return false;
+    if(hash->carga >= FACTOR_AUMENTO){
+        if(!redimensionar_hash(hash, AUMENTAR)) return false;//Le pasamos DISMINUIR si hay que achicar
     }
 
     int indice = hashing(clave, hash);
@@ -253,6 +281,11 @@ void *hash_borrar(hash_t *hash, const char *clave){
     dato = hash->tabla[posicion].valor;
     hash->tabla[posicion] = campo;
     hash->cantidad--;
+    hash->carga = ((float)hash->cantidad)/(float)hash->largo;
+
+    if(hash->carga <= FACTOR_DISMINUCION && hash->largo > TAM){
+        if(!redimensionar_hash(hash, DISMINUIR)) return false;
+    }
     return dato;
 
 }
